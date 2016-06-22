b085bf57ec638730300fe697f7c998d669835e9f
LegendTian <405186788@qq.com>
2016-06-22 18:02:26 +0800

    schoolmodel

--- javascripts/app.js
+++ javascripts/app.js
@@ -140,7 +140,7 @@ var addSchoolModel= function () {
     //ltcs.createGlbModel('./Assets/CZML/models/CesiumMan/Cesium_Man.glb',117.161, 32.71, 0);
 
 
-    //ltcs.addModel('./data/models/DX.gltf',1,118.8852508390,31.8988190356, 0,0,0,0);
+    ltcs.addModel('./data/models/DX.gltf',1,118.8852508390,31.8988190356, 0,0,0,0);
 
     /*ltcs.addModel('./data/models/JKBG.gltf',1,118.894299993421,31.900852722035, 0,0,0,0);
     ltcs.addModel('./data/models/TSG.gltf',1.1,118.8951288,31.90562233, 0,72,0,0);
@@ -157,11 +157,13 @@ var addSchoolModel= function () {
     ltcs.addModel('./data/models/NQSTHQL.gltf',1,118.897100965230,31.902720145236, 0,-19,0,0);
     ltcs.addModel('./data/models/JXL345.gltf',1,118.89449401889,31.904127874771, 0,-20,0,0);
     ltcs.addModel('./data/models/jxl678.gltf',1,118.895723872652,31.904792890584, 0,-20,0,0);
-
+     ltcs.addModel('./data/models/SS4L.gltf',1,118.898231311783,31.899717508373, 0,-20,0,0);
+     ltcs.addModel('./data/models/SS4L.gltf',1,118.896731311783,31.900868508373, 0,-20,0,0);
+     ltcs.addModel('./data/models/SS4L.gltf',1,118.896439311783,31.901578508373, 0,-20,0,0);
      */
 
-    ltcs.addModel('./data/models/jxl678.gltf',1,118.895723872652,31.904792890584, 0,-20,0,0);
 
+    ltcs.addModel('./data/models/5SSL.gltf',1,118.896439311783,31.901578508373, 0,-20,0,0);
 }
 
 var addSchoolLabel= function () {
